427b2a22de26dd96a65ac36e40d83882aa4a3333
mattramrev <matt.ramirez@revature.net>
2021-11-08 12:49:34 -0500

    populated database

--- p2script.sql
+++ p2script.sql
@@ -27,7 +27,7 @@ create table exercises
 	exerciseName varchar(50),
 	exerciseIntensity int,
 	exercisePrimaryTarget_fk int references exerciseTargetRef(exerciseTargetID), 
-	exerciseDescription varchar(50)
+	exerciseDescription varchar(500)
 	
  
 );
@@ -50,4 +50,33 @@ create table exerciseWorkoutJoin
 	workoutOrder int
 );
 	
-	
+
+insert into exercisetargetref 
+values (1, 'arms'),
+		(2, 'chest'),
+		(3, 'back'),
+		(4, 'legs'),
+		(5, 'core'),
+		(6, 'glutes'),
+		(7, 'cardio');
+
+insert into exercises 
+values  (default, 'Jumping jacks', 1, 7, 'Stand with your feet spread shoulder width apart. jump while raising your hands while widening your stance. Jump again and return to your previous position'),
+		(default, 'Warrior pose', 1, 4, 'Stand with your feet shoulder width apart. point one foot so it is perpendicular with your other foot. Point your arms outward and turn your head to the right, and bend your front knee and lean forward. Hold for 30 seconds.'),
+		(default, 'Sun salutation', 1, 3, 'stand up straight and lift your arms pointing them directly up. bend backwards, pushing out your chest. then put your arms down and lean forward so you are touching your toes. lift your arms together and bend your knees so that you are pointing upward with your glutes pushed out behind you. return to your original position'),
+		(default, 'Push-ups', 2, 2, 'Lay on the floor face forward. place your hands shoulder width apart. while keeping your legs and your back straight, push yourself off the ground, then slowly lower yourself back.'),
+		(default, 'Sit-ups', 2, 5, 'Sit on the ground with your legs bent and your feet flat on the ground. Cross your arms across your chest and slowly lean back until you back touches the ground, then lift yourself back up.'),
+		(default, 'Pull-ups', 2, 1, 'Find a sturdy bar and grip it with your fingers facing yourself. Pull yourself up so that your chin is the same height as the bar, and then lower yourself back down.'),
+		(default, 'Lunges', 3, 4, 'Stand with your legs shoulder width apart. take a large step forward, then bend your knees so that you are in a kneeling position, almost touching the ground but not quite. return to standing and repeat with your other leg.'),
+		(default, 'Squats', 3, 6, 'Stand with your feet shoulder width apart. bend your knees and crouch slowly while keeping them facing forward. keep your back straight and put your arms forward for balance. return to your original position'),
+		(default, 'Bench press', 3, 2, 'Lay back on a workout bench with a barbell. Lift the barbell so your arms are extended, and then slowly bring it down so the barbell is just above your chest. Lift back up and repeat');
+		
+insert into users 
+values	(default, 'gymleader', 'guest', true),
+		(default, 'athleticaaron', 'password', false),		
+		(default, 'builtbenjamin', 'password', false),
+		(default, 'cardiokaren', 'password', false),
+		(default, 'deadliftdarren', 'password', false),
+		(default, 'exerciseerica', 'password', false),
+		(default, 'fitfreddy', 'password', false),
+		(default, 'gameongarry', 'password', false);
